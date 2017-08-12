4c0cb98708769d0cf4f746d484aca16b1358f0c0
OneCuteLiz <liz.jenerson@gmail.com>
2017-08-12 15:33:42 +0000

    retrieving value of oil paintings key from shipping manifest hash

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,17 +8,16 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   shipping_manifest = [
-    "whale_bone_corset" => 5,
-    "porcelain_vases" => 2,
-    "oil_paintings" => 3
+    "whale_bone corset" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
   ]
 
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
