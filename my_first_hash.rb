d54463bbe5641232f8d4ab73de83780cf6bd558f
Jen Poole <contact.vandepool@gmail.com>
2017-10-28 22:31:33 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,31 +1,29 @@
+# create a hash that contains key/value pairs
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash = {
+    "day" => "Sunday",
+    "location" => "Earth"
+  }
 end
 
-
+# sets a variable called shipping_manifest equal to a hash with key/value pairs
 def shipping_manifest
-  #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 end
 
+# operates on the shipping_manifest hash to return the value of the 'oil paintings' key
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  #your code here!
+  shipping_manifest['oil paintings']
 end
 
+# operates on the shipping_manifest hash to add a key/value pair
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   
-  #your code here
-  #remember to return the shipping_manifest hash
-
+  shipping_manifest["pearl necklace"] = 1
+  return shipping_manifest
 end
-
-
-
